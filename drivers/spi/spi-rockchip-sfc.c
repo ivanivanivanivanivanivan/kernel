2cbd4fad3f103558a3b1d48579b16a02fda51b4c
Jon Lin <jon.lin@rock-chips.com>
2024-06-21 16:19:09 +0800

    spi: rockchip-sfc: Add rockchip,fspi compatible
    Change-Id: I0ccccf4061465836837cfde78bf7e70d2da78ac6
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/spi/spi-rockchip-sfc.c
+++ drivers/spi/spi-rockchip-sfc.c
@@ -1066,6 +1066,7 @@ static const struct dev_pm_ops rockchip_sfc_pm_ops = {
 };
 
 static const struct of_device_id rockchip_sfc_dt_ids[] = {
+	{ .compatible = "rockchip,fspi"},
 	{ .compatible = "rockchip,sfc"},
 	{ /* sentinel */ }
 };
