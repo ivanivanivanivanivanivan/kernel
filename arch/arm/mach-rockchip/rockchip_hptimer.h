1c4146f3c0db2d46b74aa348ae4048de5899a28a
XiaoDong Huang <derrick.huang@rock-chips.com>
2024-07-03 11:16:34 +0800

    ARM: rockchip: add hptimer_v2 functions
    Signed-off-by: XiaoDong Huang <derrick.huang@rock-chips.com>
    Change-Id: If5d2f6dfa4080583f758966782ac41f772db6d3f

--- arch/arm/mach-rockchip/rockchip_hptimer.h
+++ arch/arm/mach-rockchip/rockchip_hptimer.h
@@ -15,8 +15,17 @@ enum rk_hptimer_mode_t {
 int rk_hptimer_is_enabled(void __iomem *base);
 int rk_hptimer_get_mode(void __iomem *base);
 u64 rk_hptimer_get_count(void __iomem *base);
+
 int rk_hptimer_wait_mode(void __iomem *base, enum rk_hptimer_mode_t mode);
 void rk_hptimer_do_soft_adjust(void __iomem *base, u32 hf, u32 lf);
 void rk_hptimer_do_soft_adjust_no_wait(void __iomem *base, u32 hf, u32 lf);
 void rk_hptimer_mode_init(void __iomem *base, enum rk_hptimer_mode_t mode);
+
+int rk_hptimer_v2_wait_sync(void __iomem *base);
+void rk_hptimer_v2_do_soft_adjust(void __iomem *base, u32 hf, u32 lf);
+void rk_hptimer_v2_do_soft_adjust_no_wait(void __iomem *base, u32 hf, u32 lf);
+void rk_hptimer_v2_do_hard_adjust(void __iomem *base);
+void rk_hptimer_v2_do_hard_adjust_no_wait(void __iomem *base);
+void rk_hptimer_v2_mode_init(void __iomem *base, enum rk_hptimer_mode_t mode, u32 hf);
+
 #endif
